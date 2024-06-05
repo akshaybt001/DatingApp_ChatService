5c57bb19856f6d20f9d066c72d8533f635c7352f
Akshay Babu <akshaybabut1@gmail.com>
2024-06-05 12:57:46 +0530

    local host change to user-service

--- initializer/initializer.go
+++ initializer/initializer.go
@@ -7,10 +7,10 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-func Initializer(db *mongo.Database) *handler.ChatHandlers{
-	adapter:=adapter.NewChatAdapter(db)
-	usecase:=usecase.NewChatUsecase(adapter)
-	insertRoom:=usecase.InsertIntoDB()
-	handler:=handler.NewChatHandlers(insertRoom,&usecase,"localhost:8081")
+func Initializer(db *mongo.Database) *handler.ChatHandlers {
+	adapter := adapter.NewChatAdapter(db)
+	usecase := usecase.NewChatUsecase(adapter)
+	insertRoom := usecase.InsertIntoDB()
+	handler := handler.NewChatHandlers(insertRoom, &usecase, "user-service:8081")
 	return handler
 }
